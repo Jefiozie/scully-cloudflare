ffaa7261eaf82ccb1ab9fce7d2036654aa027ae8
Jefiozie <jefiozie.bosch@gmail.com>
2021-03-10 20:52:49 +0100

    rever ci: test

--- scully.scully-cloudflare.config.ts
+++ scully.scully-cloudflare.config.ts
@@ -6,6 +6,9 @@ export const config: ScullyConfig = {
   routes: {},
   puppeteerLaunchOptions: {
     args: [
+      '--disable-gpu',
+      '--renderer',
+      '--no-sandbox',
       '--no-service-autorun',
       '--no-experiments',
       '--no-default-browser-check',
