a9da1f6cd55fa74eb8ebfb5f88a9f26315de6474
Jefiozie <jefiozie.bosch@gmail.com>
2021-03-10 10:56:12 +0100

    chore: add scully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-{ path: '', pathMatch: 'full', redirectTo: 'home' }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
